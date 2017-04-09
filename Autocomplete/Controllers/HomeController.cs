b49270477d2ec12458fecaa0624c85dc41e4037c
Vadim Bondaruk <wadbondaruk@gmail.com>
2017-04-09 23:20:09 +0300

    Add project files.

---
+++ Autocomplete/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Autocomplete.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Autocomplete.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetData(string term)
+        {
+            var listOfPerson = new List<Person>()
+            {
+                new Person {FirstName = "Ivan", LastName="Ivanov", Id=1 },
+                new Person {FirstName = "Petr", LastName = "Petrov", Id=2 },
+                new Person {FirstName = "Vadim", LastName = "Bondaruk", Id=3 }
+            };
+
+            var result = listOfPerson.Where(p => p.LastName.IndexOf(term, StringComparison.OrdinalIgnoreCase) >= 0);
+            return Json(result);
+
+        }
+    }
+}
